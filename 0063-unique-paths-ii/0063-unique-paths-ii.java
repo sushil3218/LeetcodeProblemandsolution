76ca2bcc9d83b49f748875fecbf93ee06da1ffae
Sushil Hiwse <146084094+sushil3218@users.noreply.github.com>
2026-09-18 11:24:31 +0530

    Time: 0 ms (100%), Space: 43.5 MB (64.29%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,25 +1,39 @@
-class Solution { 
-    public static int findPaths(int i , int  j , int[][]arr ,int[][] dp){
-        if(i< 0|| j< 0 || arr[i][j] == 1 ){
-            return 0 ;
-        }
-        if(i == 0 && j == 0){
-            return 1 ;
-        }
-        if(dp[i][j] != -1){
-             return dp[i][j] ;
-        }
-        int up = findPaths( i-1 , j ,arr , dp);
-        int left =findPaths( i , j-1, arr , dp);
-        return dp[i][j] = up + left ;
-    }
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n =  obstacleGrid.length ;
-        int m = obstacleGrid[0].length ;
+class Solution {
+    public int uniquePathsWithObstacles(int[][] matrix) {
+        int n =  matrix.length ;
+        int m = matrix[0].length ;
         int[] [] dp = new int[n][m] ;
-        for(int i = 0; i< n ; i ++){
-            Arrays.fill(dp[i] , -1) ;
+        
+       for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                
+                // Base conditions
+                if (matrix[i][j] == 1) {
+                    // If there's an obstacle, no paths can pass through it
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    // Starting point has exactly one path
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                // Check if we can move up and left
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+
+                // Sum of paths from above and left
+                dp[i][j] = up + left;
+            }
         }
-      return findPaths(n-1 , m-1 , obstacleGrid , dp);
+
+        // The answer is at the bottom-right cell
+        return dp[n - 1][m - 1];
     }
 }
