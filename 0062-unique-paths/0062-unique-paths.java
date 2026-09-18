e12ee7e84bc2162be45dd59722497a18837fcbed
Sushil Hiwse <146084094+sushil3218@users.noreply.github.com>
2026-09-18 10:47:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.54%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution { 
+    public static int findPaths(int i, int j , int[][] dp){
+        if(i == 0 && j == 0){
+            return 1 ;
+        } 
+        if(i < 0 || j< 0){
+            return 0 ;
+        }  
+        if(dp[i][j] != -1){
+            return dp[i][j] ;
+        }
+        int up = findPaths(i-1 , j ,dp) ;
+        int left = findPaths(i , j-1, dp) ;
+        return dp[i] [j] = up + left ;
+    }
+        public int uniquePaths(int m, int n) {
+        int [] []  dp = new int [m] [n];
+        for(int i = 0; i<m; i++){
+            Arrays.fill(dp[i] ,-1) ;
+        }
+       return findPaths(m-1 , n-1 , dp);
+    }
+}
